9a3816dc11473c1367a8c275cb4b950ef7edc9b3
ypares <ypares@tele2.fr>
2008-05-04 12:07:23 +0000

    Corrections d'erreurs d'indentation

--- perso.py
+++ perso.py
@@ -15,43 +15,43 @@ class Personnage:
     
     def __init__(self): 
         """Définit les attributs (stats, etc.) du personnage"""
-
+        
         self.nom = ""
-
+        
         self.stats = { "FCP": 0, "FCM": 0, "DFP": 0, "DFM": 0, "AGI": 0, "INI": 0, "MVT": 0 }
-
+        
         self.VAL = 0 # VALEUR
         self.VIE = 100.0
         self.FTG = 0.0
         # VAL, VIE et FTG ne sont pas mises dans les stats car :
         # - VAL est recalculée (afin d'éviter au maximum la triche)
         # - VIE et FTG démarrent toujours à des valeurs dépendant des maîtrises (le plus souvent 100 et 0)
-
+        
         self.maitrises = [] # Liste des maitrises (objets de type Maitrise) possédées par le perso, on initialise avec la maitrise trouvée par le questionnaire
-
+        
         self.equipement = { "bras_droit": None, "bras_gauche": None, "tete": None, "torse": None, "pieds": None, "acc1": None, "acc2": None, "special": None }
         # acc1 et acc2 sont les accessoires (colliers, bracelets pour booster), et special est un objet spécial (cheval, catapulte) dépendant généralement d'une maîtrise particulière
         # Tous les items équipés sont des instances de la classe ObjetEquip
-
+        
         self.sprite = None
     
     
     def ajMaitrise(self, maitrise,grade):
         """ Ajoute une maitrise à la liste de maitrise du personnage"""
         self.maitrises.append([maitrise, grade]) #maitrise de type maitrises
-	
-	def detMaitrise(self,indiceMaitrise):
-                """ Détruis une maitrise existante"""
-                if (indiceMaitrise >= 0) and (indiceMaitrise < len(self.maitrise)):
-                        self.maitrise.remove(indiceMaitrise)
-	
-	def changeGrade(self,indiceMaitrise,grade):
-		""" Change le grade d'une maitrise donnée """
-		self.maitrises[indiceMaitrise][1] = grade
-	
-	def changeNom(self,nom):
-		self.nom = nom
-	
+    
+    def detMaitrise(self,indiceMaitrise):
+        """ Détruit une maitrise existante"""
+        if (indiceMaitrise >= 0) and (indiceMaitrise < len(self.maitrise)):
+            self.maitrise.remove(indiceMaitrise)
+    
+    def changeGrade(self,indiceMaitrise,grade):
+        """ Change le grade d'une maitrise donnée """
+        self.maitrises[indiceMaitrise][1] = grade
+
+    def changeNom(self,nom):
+        self.nom = nom
+
     def importerDepuisXML(self, doc):
         """Charge le perso depuis un minidom.Document"""
         root = doc.documentElement
