7fb2cafda8b409a58924dacd0fecf6a8433f957c
ypares@tele2.fr <ypares@tele2.fr@02b6c007-4148-0410-a178-85d4fdf5ef18>
2008-05-05 15:19:51 +0000

    Modifs sur le moteur graphique (changement de nom, entre autres) => WS !

---
+++ trunk/WS_compil/setup.py
@@ -0,0 +1,22 @@
+# encoding=utf-8
+
+from distutils.core import setup, Extension
+import glob, os.path
+
+
+listeSources = glob.glob(os.path.join("src","*"))
+
+
+setup(
+name = 'Weltanschauung',
+version = '1.0',
+description = 'Moteur graphique pour Schpanzerbruck',
+license = 'GPL v3',
+ext_modules = [
+                Extension('PyWS._welt',
+                    ["welt_wrap.cxx"] + listeSources, \
+                    libraries=['sfml-graphics', 'sfml-window', 'sfml-audio', 'sfml-system', 'GL', 'GLU'])
+              ],
+packages = ["PyWS"],
+package_dir = {"PyWS":"PyWS"}
+)
